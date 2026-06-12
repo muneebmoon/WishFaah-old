de94afc18aba3285fb73e9dd2c59867c0a4336b5
Muneeb Ahmed <muneeb.ahmed@organix-it.com>
2026-06-12 17:54:52 +0500

    update backend

--- backend/src/main/java/pk/wishfaah/backend/product/repository/ProductRepository.java
+++ backend/src/main/java/pk/wishfaah/backend/product/repository/ProductRepository.java
@@ -1,7 +1,5 @@
 package pk.wishfaah.backend.product.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pk.wishfaah.backend.product.entity.Product;
